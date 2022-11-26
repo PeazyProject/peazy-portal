85f78a4824128f55580997f5b09bf9f24a1715de
kewos554321 <410413042@gms.ndhu.edu.tw>
2022-11-27 02:44:37 +0800

    Modify authUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,7 @@ export const environment = {
   loggingLevel: 0,
   // supplierApiUrl: 'http://localhost:8080/supplier',
   supplierApiUrl: 'https://peazy-supplier.onrender.com/supplier',
-  authUrl: 'http://localhost:8080/auth'
+  // authUrl: 'http://localhost:8080/auth'
+  authUrl: 'https://peazy-supplier.onrender.com/auth'
+
 };
