bb0358b20d1e49461b5d27eac955cf746e127a2a
JayWang <jay.wang@wpgholdings.com>
2023-09-07 23:28:17 +0800

    Jay: 增加customerApiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   // supplierApiUrl: 'http://localhost:8080/supplier',
   supplierApiUrl: 'https://peazy-supplier.onrender.com/supplier',
   // authUrl: 'http://localhost:8080/auth'
-  authUrl: 'https://peazy-auth.onrender.com/auth'
+  authUrl: 'https://peazy-auth.onrender.com/auth',
+  customerApiUrl: 'https://peazy-csutomer.onrender.com/csutomer',
 
 };
